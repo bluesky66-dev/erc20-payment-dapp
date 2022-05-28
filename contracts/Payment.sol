c6eab4bb54fc9e79dbe1694e71335ff6aee137fd
LeonSoft <newthemeground@gmail.com>
2022-05-28 22:35:46 +0530

    hardhat setup done

---
+++ contracts/Payment.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+contract NCPay is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor(address[] memory minter, address[] memory burner) ERC20("NCToken", "NCT") public{
+        for(uint i=0; i<minter.length; ++i){
+            _setupRole(MINTER_ROLE, minter[i]);
+        }
+        for(uint i=0; i<burner.length; ++i){
+            _setupRole(BURNER_ROLE, burner[i]);
+        }
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _mint(msg.sender, 1000000);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+}
